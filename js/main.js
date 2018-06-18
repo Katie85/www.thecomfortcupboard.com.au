7e0ef2d64cf8bbb690ea59008357b21ccd3b7b9e
Katie Smith <teedek@hotmail.com>
2018-06-18 22:22:53 +0800

    Responsive

--- js/main.js
+++ js/main.js
@@ -6,5 +6,10 @@ $('document').ready(function(){
 
     console.log("hello");
 
+  $(".burger-button").click(function(){
+  $(".burger-button").toggleClass("active");
+  $(".burger-menu").slideToggle();
+});
+
 
 });
